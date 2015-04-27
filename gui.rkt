a422de649e932ca2d4ea122fd755eef863ce0f3f
nick01720 <nick01720@users.noreply.github.com>
2015-04-27 10:47:12 -0400

    Update gui.rkt
    Added the xcolor and ycolor options.

--- gui.rkt
+++ gui.rkt
@@ -28,26 +28,48 @@
 ;Create function input text-fields:
 (define i-func1 (new text-field% 
                      [label "Enter function here: "]
-                     [parent frame]))
-                     ;[init-value "(lambda(x) (* x x))"]))
+                     [parent frame]
+                     [init-value "(lambda(x) (* x x))"]))
 (define i-func2 (new text-field% 
                      [label "Enter second function here: "]
-                     [parent frame]))
-                     ;[init-value "(lambda(x) (x))"]))
+                     [parent frame]
+                     [init-value "(lambda(x) x)"]))
 
 ;Create min and max text-fields for bounds of Cartesian plane:
 (define i-xmin (new text-field%
                     [label "x-min: "]
-                    [parent panel]))
+                    [parent panel]
+                    [init-value "-10"]))
 (define i-xmax (new text-field%
                     [label "x-max: "]
-                    [parent panel]))
+                    [parent panel]
+                    [init-value "10"]))
 (define i-ymin (new text-field%
                     [label "y-min: "]
-                    [parent panel]))
+                    [parent panel]
+                    [init-value "-10"]))
 (define i-ymax (new text-field%
                     [label "y-max: "]
-                    [parent panel]))
+                    [parent panel]
+                    [init-value "10"]))
+(define i-xcolor (new text-field%
+                    [label "x-color: "]
+                    [parent panel]
+                    [init-value "1"]))
+
+;Working on a drop down menu for color selection
+;(define i-xcolor (new choice%
+;                    [selection 0]
+;                      (label "x-color: ")
+;                    (parent panel)
+;                    (choices (list "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10"))))
+
+(define i-ycolor (new text-field%
+                    [label "y-color: "]
+                    [parent panel]
+                    [init-value "2"]))
+
+
 
 ;A list of provides:
 (provide frame)
@@ -56,5 +78,7 @@
 (provide i-func2)
 (provide i-xmin)
 (provide i-xmax)
+(provide i-xcolor)
 (provide i-ymin)
 (provide i-ymax)
+(provide i-ycolor)
