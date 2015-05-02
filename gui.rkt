7ee48ece24b0300341d7924fdf35d4c4a6f16cdd
Emily Seto <emily_seto@student.uml.edu>
2015-05-02 11:21:58 -0400

    Added the ability to choose up to five functions to graph 2D. Must use checkboxes to indicate whether that function is to be plotted. Added colors for each of the five functions.

--- gui.rkt
+++ gui.rkt
@@ -7,55 +7,158 @@
 ;Make a new frame by instantiating the frame% class.
 (define frame (new frame%
                    [label "Graphic-al Emulation Program (GrEP)"]
-                   [height 150]
-                   [width 350]))
+                   [height 205]
+                   [width 545]))
                    ;[style 'fullscreen-button]))
 
 ;Create a static default message in the frame.
 (define msg (new message% [parent frame] [label "STATUS MESSAGE: Testing in progress..."]))
 
 ;Define a menu bar (for funsies!):
-(define menu-bar (new menu-bar% [parent frame]))
-(new menu% [label "&File"] [parent menu-bar])
-(new menu% [label "&Edit"] [parent menu-bar])
-(new menu% [label "&Help"] [parent menu-bar])
+;(define menu-bar (new menu-bar% [parent frame]))
+;(new menu% [label "&File"] [parent menu-bar])
+;(new menu% [label "&Edit"] [parent menu-bar])
+;(new menu% [label "&Help"] [parent menu-bar])
 
-;Define a new horizontal panel.
+;Define new horizontal panels.
 (define panel (new horizontal-panel% 
                    [parent frame]
                    [alignment '(center center)]))
 
+(define panel-2 (new horizontal-panel%
+                     [parent frame]
+                     [alignment '(center center)]))
+
+(define panel-3 (new horizontal-panel%
+                     [parent frame]
+                     [alignment '(center center)]))
+
+(define panel-4 (new horizontal-panel%
+                     [parent frame]
+                     [alignment '(center center)]))
+                   
+
+(define panel-5 (new horizontal-panel%
+                     [parent frame]
+                     [alignment '(center center)]))
+
+(define panel-6 (new horizontal-panel%
+                     [parent frame]
+                     [alignment '(center center)]))
+
+(define panel-7 (new horizontal-panel%
+                     [parent frame]
+                     [alignment '(center center)]))
+
+(define panel-8 (new horizontal-panel%
+                     [parent frame]
+                     [alignment '(center center)]))
+(define panel-9 (new horizontal-panel%
+                     [parent frame]
+                     [alignment '(center center)]))
+                   
+                   
+              
+                   
+
+;Create new dropdown menus:
+;(define n-functs (new choice% 
+;                        (label "Number of Functions: ")
+;                        (parent panel)
+;                        (choices (list "1" "2" "3" "4" "5"))))
+
+;(define n-colors (new choice% 
+;                        (label "Colors: ")
+;                        (parent panel)
+;                        (choices (list "Item 0" "Item 1" "Item 2"))))
+
 ;Create function input text-fields:
 (define i-func1 (new text-field% 
-                     [label "Enter function here: "]
-                     [parent frame]
+                     [label "Enter 1st function here: "]
+                     [parent panel-5]
                      [init-value "(lambda(x) (* x x))"]))
+(define i-color1 (new text-field%
+                    [label "Color: "]
+                    [parent panel-5]
+                    [init-value "1"]))
+(define func1-cb (new check-box%
+                      (parent panel-5)
+                      (label "Use")
+                      (value #t)))
+
+
 (define i-func2 (new text-field% 
-                     [label "Enter second function here: "]
-                     [parent frame]
+                     [label "Enter 2nd function here: "]
+                     [parent panel-6]
                      [init-value "(lambda(x) x)"]))
+(define i-color2 (new text-field%
+                    [label "Color: "]
+                    [parent panel-6]
+                    [init-value "2"]))
+(define func2-cb (new check-box%
+                      (parent panel-6)
+                      (label "Use")
+                      (value #t)))
+
+(define i-func4 (new text-field% 
+                     [label "Enter 3rd function here: "]
+                     [parent panel-7]))
+                     ;[init-value "(lambda(x) x)"]))
+(define i-color4 (new text-field%
+                    [label "Color: "]
+                    [parent panel-7]
+                    [init-value "2"]))
+(define func4-cb (new check-box%
+                      (parent panel-7)
+                      (label "Use")
+                      (value #f)))
+
+(define i-func5 (new text-field% 
+                     [label "Enter 4th function here: "]
+                     [parent panel-8]))
+(define i-color5 (new text-field%
+                    [label "Color: "]
+                    [parent panel-8]
+                    [init-value "2"]))
+(define func5-cb (new check-box%
+                      (parent panel-8)
+                      (label "Use")
+                      (value #f)))
+
+(define i-func6 (new text-field% 
+                     [label "Enter 5th function here: "]
+                     [parent panel-9]))
+(define i-color6 (new text-field%
+                    [label "Color: "]
+                    [parent panel-9]
+                    [init-value "2"]))
+(define func6-cb (new check-box%
+                      (parent panel-9)
+                      (label "Use")
+                      (value #f)))
+
 
 ;Create min and max text-fields for bounds of Cartesian plane:
 (define i-xmin (new text-field%
                     [label "x-min: "]
-                    [parent panel]
+                    [parent panel-2]
                     [init-value "-10"]))
 (define i-xmax (new text-field%
                     [label "x-max: "]
-                    [parent panel]
+                    [parent panel-2]
                     [init-value "10"]))
 (define i-ymin (new text-field%
                     [label "y-min: "]
-                    [parent panel]
+                    [parent panel-2]
                     [init-value "-10"]))
 (define i-ymax (new text-field%
                     [label "y-max: "]
-                    [parent panel]
+                    [parent panel-2]
                     [init-value "10"]))
-(define i-xcolor (new text-field%
-                    [label "x-color: "]
-                    [parent panel]
-                    [init-value "1"]))
+;(define i-xcolor (new text-field%
+;                    [label "x-color: "]
+;                    [parent panel-3]
+;                    [init-value "1"]))
 
 ;Working on a drop down menu for color selection
 ;(define i-xcolor (new choice%
@@ -64,11 +167,10 @@
 ;                    (parent panel)
 ;                    (choices (list "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10"))))
 
-(define i-ycolor (new text-field%
-                    [label "y-color: "]
-                    [parent panel]
-                    [init-value "2"]))
-
+;(define i-ycolor (new text-field%
+;                    [label "y-color: "]
+;                    [parent panel-3]
+;                    [init-value "2"]))
 
 
 ;A list of provides:
@@ -76,9 +178,24 @@
 (provide msg)
 (provide i-func1)
 (provide i-func2)
+;(provide i-func3)
+(provide i-func4)
+(provide i-func5)
+(provide i-func6)
 (provide i-xmin)
 (provide i-xmax)
-(provide i-xcolor)
+;(provide i-xcolor)
 (provide i-ymin)
 (provide i-ymax)
-(provide i-ycolor)
+;(provide i-ycolor)
+(provide func1-cb)
+(provide func2-cb)
+;(provide func3-cb)
+(provide func4-cb)
+(provide func5-cb)
+(provide func6-cb)
+(provide i-color1)
+(provide i-color2)
+(provide i-color4)
+(provide i-color5)
+(provide i-color6)
--- plot.rkt
+++ plot.rkt
@@ -6,6 +6,8 @@
 
 (require "gui.rkt")
 
+(define nil '())
+
 ;func1 is going to be the text in the textbox.
 ;Adjust func1 using a set! procedure.
 (define func1 (λ(x) (* 2 x)))
@@ -32,35 +34,51 @@
      [label "Update!"]
      ;Creates a callback procedure for a button click:
      [callback (λ(button event)
-               (define n-labl
-                 (send msg set-label (string-append "func: " (send i-func1 get-value))))
+                 (define n-labl (send msg set-label (string-append "func: " (send i-func1 get-value))))
                  (display n-func)
                  ;Capture whatever is in the min and max x,y values:
                  (define xmin (send i-xmin get-value))
                  (define xmax (send i-xmax get-value))
                  (define ymin (send i-ymin get-value))
                  (define ymax (send i-ymax get-value))
+                 ;Capures state of checkboxes.
+                 (define (fun1?) (send func1-cb get-value))
+                 (define (fun2?) (send func2-cb get-value))
+                 (define (fun4?) (send func4-cb get-value))
+                 (define (fun5?) (send func5-cb get-value))
+                 (define (fun6?) (send func6-cb get-value))
                  (send msg set-label "STATUS MESSAGE: Updating the function...")
-                ;(let* ([
-                 (plot (list (axes)
-                             ;(function (lambda (x) (0)) -2 2)
-                             (function (eval (read (open-input-string (send i-func1 get-value)))) #:label (send i-func1 get-value) 
-                                       #:color (read (open-input-string(send i-xcolor get-value))))
-                             (function (eval (read (open-input-string (send i-func2 get-value)))) #:label (send i-func2 get-value) 
-                                       #:color (read (open-input-string(send i-ycolor get-value)))))
+                 (let ([functions (filter (λ(x) (not (null? x))) 
+                                          (list
+                                           (if (fun1?)
+                                               (function (eval (read (open-input-string (send i-func1 get-value)))) #:label (send i-func1 get-value) 
+                                                         #:color (read (open-input-string (send i-color1 get-value)))) nil)
+                                           (if (fun2?)
+                                               (function (eval (read (open-input-string (send i-func2 get-value)))) #:label (send i-func2 get-value) 
+                                                         #:color (read (open-input-string (send i-color2 get-value)))) nil)
+                                           (if (fun4?)
+                                               (function (eval (read (open-input-string (send i-func4 get-value)))) #:label (send i-func4 get-value) 
+                                                         #:color (read (open-input-string (send i-color4 get-value)))) nil)
+                                           (if (fun5?)
+                                               (function (eval (read (open-input-string (send i-func5 get-value)))) #:label (send i-func5 get-value) 
+                                                         #:color (read (open-input-string (send i-color5 get-value)))) nil)
+                                           (if (fun6?)
+                                               (function (eval (read (open-input-string (send i-func6 get-value)))) #:label (send i-func6 get-value) 
+                                                         #:color (read (open-input-string (send i-color6 get-value)))) nil)))])
+                 (plot (append (list (axes)) functions)
                         #:x-min (eval (read (open-input-string xmin)))
                         #:x-max (eval (read (open-input-string xmax)))
                         #:y-min (eval (read (open-input-string ymin))) 
-                        #:y-max (eval (read (open-input-string ymax)))
-                        )
-                 (parameterize ([plot-title  "TEST 3D"]
+                        #:y-max (eval (read (open-input-string ymax))))
+                 ; For 3D graphing:
+                 (parameterize ([plot-title  "Testing 3D plotting..."]
                                 [plot-x-label "x"]
                                 [plot-y-label "y"]
                                 [plot-z-label "cos(x) sin(y)"])
                    (plot3d (contour-intervals3d (eval (read (open-input-string (send i-func3 get-value))))
                                                 (- pi) pi (- pi) pi)))
-                 (send frame show #t)
-                 (send msg set-label "STATUS MESSAGE: Updated"))])
+                 ;(send frame show #t)
+                 (send msg set-label "STATUS MESSAGE: Updated")))])
 
 
 ;this works!!!!!!!!!!!!!!!!!
